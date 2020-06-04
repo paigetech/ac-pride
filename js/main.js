03d77cdfec9cd72f537b50c654142a14f0aaa494
Paige Scott <paige.scott@stitchfix.com>
2020-06-04 12:38:02 -0700

    first draft

--- js/main.js
+++ js/main.js
@@ -70,7 +70,7 @@
     /*------------------
         CountDown
     --------------------*/
-	$("#countdown").countdown("2019/12/25", function(event) {
+	$("#countdown").countdown("2020/07/25", function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
     });
 
